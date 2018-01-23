bcdd62e80ccb44cebb90a8f24e06b1281d8a2d68
sup <sup@engr.tu.ac.th>
2018-01-23 14:41:26 +0700

    Move preamble back into original tex file

--- auto/me210.el
+++ auto/me210.el
@@ -3,19 +3,53 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("book" "10pt" "a4paper" "openany" "svgnames" "")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("parskip" "parfill") ("tocloft" "titles" "subfigure") ("titlesec" "explicit") ("biblatex" "style=numeric" "backend=biber")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
-    "me210style"
     "book"
-    "bk10")
+    "bk10"
+    "inputenc"
+    "geometry"
+    "graphicx"
+    "parskip"
+    "booktabs"
+    "array"
+    "enumitem"
+    "verbatim"
+    "subfig"
+    "fancyhdr"
+    "tocloft"
+    "newtxtext"
+    "newtxmath"
+    "hyperref"
+    "amsmath"
+    "amsthm"
+    "amssymb"
+    "multirow"
+    "gensymb"
+    "cleveref"
+    "siunitx"
+    "titlesec"
+    "tikz"
+    "calc"
+    "thmtools"
+    "mdframed"
+    "float"
+    "biblatex")
    (TeX-add-symbols
+    '("AxisRotator" ["argument"] 0)
+    "chapterlabel"
+    "exercise"
+    "openbox"
+    "newline"
     "arraystretch")
    (LaTeX-add-labels
     "fig: 3d-stress-element"
@@ -85,6 +119,13 @@
     "section: buckling"
     "eqn: gen solution"
     "eqn: mode shape sub"
-    "eqn: Euler's formula"))
+    "eqn: Euler's formula")
+   (LaTeX-add-bibliographies)
+   (LaTeX-add-array-newcolumntypes
+    "L"
+    "C"
+    "R")
+   (LaTeX-add-enumitem-newlists
+    '("exercises" "enumerate")))
  :latex)
 
